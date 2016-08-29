9b13a53b3cd3e1e18aac93717aa6ac099f9f11b2
jdercole1 <jdercole1@L11116042798.wctc.msft>
2016-08-29 18:44:30 -0500

    Modified main method and added new method.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -9,8 +9,12 @@ public class FirstGit {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("never do more work than you're willing to lose");
+        System.out.println("something else");
+        
+        
+        
     }
+    public void method2() {
     
+    }
 }
