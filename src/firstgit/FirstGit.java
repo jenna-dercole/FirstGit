f05f60a06b0e5d2532441312bb4e512ba3f098ae
jdercole1 <jdercole1@L11116042798.wctc.msft>
2016-08-29 18:37:31 -0500

    First commit

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,15 @@
+
+package firstgit;
+
+/**
+ *
+ * @author jdercole1
+ */
+public class FirstGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
