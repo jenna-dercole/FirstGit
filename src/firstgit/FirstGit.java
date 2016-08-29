1887bddedc0f29cfb20027fab7afc1bba2db2d04
jdercole1 <jdercole1@L11116042798.wctc.msft>
2016-08-29 18:38:58 -0500

    First commit

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -10,6 +10,7 @@ public class FirstGit {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("never do more work than you're willing to lose");
     }
     
 }
